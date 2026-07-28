41374835c58e11e68caa0f2a06218e0bd7f5a825
MUHD-UNAIS <unaispoonthala8086@gmail.com>
2026-07-28 22:06:00 +0530

    the blockchain exlpore option in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import RegisterLand from "./components/RegisterLand";
-
+import BlockchainExplorer from "./components/BlockchainExplorer";
 function App() {
     return (
         <div>
             <h1>Land Blockchain System</h1>
 
             <RegisterLand />
+
+            <hr />
+
+            <BlockchainExplorer />
         </div>
     );
 }
