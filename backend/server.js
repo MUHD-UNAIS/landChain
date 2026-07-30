701700718c38e2155519ab17a4b00b86b3d5d701
MUHD-UNAIS <unaispoonthala8086@gmail.com>
2026-07-30 21:30:22 +0530

    db created

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 
+const connectDB=require("./database")
+
 const Blockchain = require("./blockchain");
 
 const app = express();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
